d77dc63c0fd7462923e905095d5ff8a8fe537914
Jacob McGowan <73966737+TheDevAtlas@users.noreply.github.com>
2023-06-01 21:56:12 -0400

    ChartTest

--- 2.0.0 - IRIS Unity/Assets/Scripts/GraphBuilder.cs
+++ 2.0.0 - IRIS Unity/Assets/Scripts/GraphBuilder.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandlestickChart : MonoBehaviour
+public class GraphBuilder : MonoBehaviour
 {
-    public float chartWidth = 10f;
-    public float chartHeight = 5f;
-    public float candleWidth = 0.5f;
-    public int numCandle = 0;
     public Sprite candleSprite;
+    public float candleSeperation;
 
-    void Start()
+    public Vector4[] data;
+
+    private void Start()
     {
-        CreateCandlestick(150f, 170f, 180f, 140f, (candleWidth + 0.15f) * (float)numCandle);  // Example call for a single candlestick
-        numCandle++;
-        CreateCandlestick(160f, 155f, 170f, 150f, (candleWidth + 0.15f) * (float)numCandle);  // Example call for another candlestick
+        CreateNewChart();
+    }
+    void CreateNewChart()
+    {
+        float num = 0;
+        foreach(Vector4 d in data)
+        {
+            CreateCandlestick(d.x, d.y, d.z, d.w, num);
+            num++;
+        }
     }
 
     void CreateCandlestick(float openPrice, float closePrice, float highPrice, float lowPrice, float num)
@@ -22,13 +28,13 @@ public class CandlestickChart : MonoBehaviour
         float minPrice = Mathf.Min(openPrice, closePrice, highPrice, lowPrice);
         float maxPrice = Mathf.Max(openPrice, closePrice, highPrice, lowPrice);
         float priceRange = maxPrice - minPrice;
-        float verticalScale = chartHeight / priceRange;
+        float verticalScale = transform.localScale.y / priceRange;
 
         GameObject candlestick = new GameObject("Candlestick");
-        candlestick.transform.SetParent(transform);
 
-        candlestick.transform.localPosition = new Vector3(chartWidth / 2f + num, (openPrice - minPrice) * verticalScale / 2f, 0f);
-        candlestick.transform.localScale = new Vector3(candleWidth, (closePrice - openPrice) * verticalScale, 1f);
+        float x = transform.localScale.x / data.Length / 2f;
+        candlestick.transform.localPosition = new Vector3(num - transform.localScale.x / 2f, (openPrice - minPrice) * verticalScale / 2f, 0f);
+        candlestick.transform.localScale = new Vector3(0.2f, (closePrice - openPrice) * verticalScale, 1f);
 
         SpriteRenderer spriteRenderer = candlestick.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = candleSprite;
@@ -38,10 +44,13 @@ public class CandlestickChart : MonoBehaviour
         lineRenderer.positionCount = 2;
         lineRenderer.startWidth = 0.02f;
         lineRenderer.endWidth = 0.02f;
-        lineRenderer.SetPosition(0, new Vector3(chartWidth / 2f + num, (highPrice - minPrice) * verticalScale / 2f, 0f));
-        lineRenderer.SetPosition(1, new Vector3(chartWidth / 2f + num, (lowPrice - minPrice) * verticalScale / 2f, 0f));
+        lineRenderer.SetPosition(0, new Vector3(num - transform.localScale.x / 2f, (highPrice - minPrice) * verticalScale / 2f, 0f));
+        lineRenderer.SetPosition(1, new Vector3(num - transform.localScale.x / 2f, (lowPrice - minPrice) * verticalScale / 2f, 0f));
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.startColor = (closePrice > openPrice) ? Color.green : Color.red;
         lineRenderer.endColor = (closePrice > openPrice) ? Color.green : Color.red;
+
+        candlestick.transform.SetParent(transform);
+
     }
 }
